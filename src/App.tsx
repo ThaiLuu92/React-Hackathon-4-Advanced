c8c45b62fe0fd69dd3c55122728877aced2abb95
ThaiLuu92 <matienjp92@gmail.com>
2023-10-05 22:10:35 +0700

    update

--- src/App.tsx
+++ src/App.tsx
@@ -5,61 +5,112 @@ import { ProductList, Product } from "./Data/data";
 import Cart from "./Data/Component/Cart";
 
 function App() {
-  const [isCartOpen, setIsCartOpen] = useState(false);
+  const [isCartOpen, setIsCartOpen] = useState<boolean>(false);
+  const [cartItems, setCartItems] = useState<Product[]>([]);
+  const [selectedProducts, setSelectedProducts] = useState<(Product | null)[]>([]);
 
- 
   const toggleCart = () => {
     setIsCartOpen(!isCartOpen);
   };
+
   const closeCart = () => {
     setIsCartOpen(false);
-  }
-  
+  };
 
+  const addToCart = (index: number) => {
+    const productToAdd = ProductList[index];
+
+    if (productToAdd) {
+      const existingItem = cartItems.find((item) => item.name === productToAdd.name);
+
+      if (existingItem) {
+        const updatedCart = cartItems.map((item) =>
+          item.name === productToAdd.name
+            ? { ...item, qty: item.qty + 1 }
+            : item
+        );
+        setCartItems(updatedCart);
+      } else {
+        setCartItems([...cartItems, { ...productToAdd, qty: 1 }]);
+      }
+    }
+  };
+
+
+
+  // Tính tổng số lượng sản phẩm trong giỏ hàng
+  const totalCartQuantity = cartItems.reduce(
+    (total, item) => total + item.qty,
+    0
+  );
 
   return (
     <div className="App">
       <header>
         <FaShoppingCart className="icon-cart" onClick={toggleCart} />
-        <div id="total-item">0
-        </div>
+        {/* Hiển thị tổng số lượng sản phẩm */}
+        <div id="total-item">{totalCartQuantity}</div>
       </header>
       <main>
-      {ProductList.map((product) => (
-        <div className="product-card">
-          <div className="product-stock">
-            <i>
-              <FaApple className="icon" />
-            </i>
-            <p>In Stock</p>
-          </div>
-          <div className="product-image">
-            <img
-              src={product.imgUrl}
-              alt=""
-            />
-          </div>
-          <div className="product-info">
-            <div className="product-name">
-              <h3>{product.name}</h3>
+        {ProductList.map((product, index) => (
+          <div className="product-card" key={product.name}>
+            <div className="product-stock">
               <i>
                 <FaApple className="icon" />
               </i>
+              <p>In Stock</p>
             </div>
-            <div className="product-price">
-              <h4>{product.price}</h4>
-              <button className="btn btb-add">Add</button>
+            <div className="product-image">
+              <img src={product.imgUrl} alt={product.name} />
             </div>
+            <div className="product-info">
+              <div className="product-name">
+                <h3>{product.name}</h3>
+                <i>
+                  <FaApple className="icon" />
+                </i>
+              </div>
+              <div className="product-price">
+                <h4>{product.price}</h4>
+                <div className="quantity-control">
+                  <button
+                    className="btn btn-decrement"
+                  
+                  >
+                    -
+                  </button>
+                  {/* Hiển thị số lượng sản phẩm của sản phẩm đang chọn */}
+                  <span>{selectedProducts[index]?.qty || 0}</span>
+                  <button
+                    className="btn btn-increment"
+                    
+                  >
+                    +
+                  </button>
+                </div>
+                <button
+                  className="btn btb-add"
+                  onClick={() => {
+                    addToCart(index);
+                  }}
+                >
+                  Add
+                </button>
+              </div>
 
-            <p>
-              {product.des}
-            </p>
+              <p>{product.des}</p>
+            </div>
           </div>
-        </div>
-      ))}
+        ))}
       </main>
       <div className="cart">
-        {isCartOpen && <Cart onClick={closeCart} />}
+        {isCartOpen && (
+          <Cart
+            cartItems={cartItems}
+            onClick={closeCart}
+            setSelectedProduct={setCartItems}
+          />
+        )}
       </div>
     </div>
   );
--- src/Data/Component/Cart.tsx
+++ src/Data/Component/Cart.tsx
@@ -1,13 +1,48 @@
-import React from 'react'
+import { ProductList,Product } from "../data";
+import React, { useState } from "react";
 import "./style.css"
-interface Props{
- onClick: () => void
+
+
+
+
+interface CartProps {
+    cartItems: Product[];
+    onClick: () => void;
+    setSelectedProduct: React.Dispatch<React.SetStateAction<Product[]>>; // Cập nhật kiểu này
 }
-function Cart(props: Props) {
+
+function Cart({ cartItems, onClick, setSelectedProduct }: CartProps) {
+    const [showBuyConfirmation, setShowBuyConfirmation] = useState(false);
+    const [showPurchaseSuccess, setShowPurchaseSuccess] = useState(false);
+
+    const handleBuyClick = () => {
+        setShowBuyConfirmation(true);
+    };
+
+    const confirmBuy = () => {
+        // Xử lý xác nhận mua ở đây
+        setShowBuyConfirmation(false);
+        setShowPurchaseSuccess(true);
+    };
+
+    const closeModal = () => {
+        setShowBuyConfirmation(false);
+        setShowPurchaseSuccess(false);
+    };
+
+    
+    // Tính tổng giá tiền trong giỏ hàng
+    const totalAmount = cartItems.reduce(
+        (total, item) => total + item.price * item.qty,
+        0
+    );
+    const clearCart = () => {
+        setSelectedProduct([]); // Đặt giỏ hàng là mảng rỗng để xóa hết sản phẩm
+    };
+
     return (
-      
-            <div id="page-cart">
-                <div className="container">
+        <div id="page-cart">
+            <div className="container">
                 <table id="cart">
                     <thead>
                         <tr>
@@ -19,46 +54,90 @@ function Cart(props: Props) {
                         </tr>
                     </thead>
                     <tbody>
-
-                        <tr className="productitm">
-
-                            <td>
-                                <img src="https://purepng.com/public/uploads/large/apple-watch-pcq.png" className="thumb" />
-                            </td>
-                            <td>
-                                <p>Tên</p>
-                            </td>
-                            <td>
-                                <div className='td-qyt'> <button>-</button><span>0</span><button>+</button></div>
-                            </td>
-                          
-                            <td>$79.00</td>
-                          
-                          
-                            <td><button>Xóa</button></td>
-                        </tr>
+                        {cartItems.map((product) => (
+                            <tr className="productitm" key={product.name}>
+                                <td>
+                                    <img src={product.imgUrl} className="thumb" alt={product.name} />
+                                </td>
+                                <td>
+                                    <p>{product.name}</p>
+                                </td>
+                                <td>
+                                    <div className='td-qyt'>
+                                        <button onClick={() => {
+                                            const updatedCart = cartItems.map(item => {
+                                                if (item === product && item.qty > 1) {
+                                                    return { ...item, qty: item.qty - 1 };
+                                                }
+                                                return item;
+                                            });
+                                            setSelectedProduct(updatedCart);
+                                        }}>-</button>
+                                        <span>{product.qty}</span>
+                                        <button onClick={() => {
+                                            const updatedCart = cartItems.map(item => {
+                                                if (item === product) {
+                                                    return { ...item, qty: item.qty + 1 };
+                                                }
+                                                return item;
+                                            });
+                                            setSelectedProduct(updatedCart);
+                                        }}>+</button>
+                                    </div>
+                                </td>
+                                <td>${(product.price * product.qty).toFixed(2)}</td>
+                                <td>
+                                    <button onClick={() => {
+                                        // Xóa sản phẩm khỏi giỏ hàng
+                                        const updatedCart = cartItems.filter(item => item !== product);
+                                        setSelectedProduct(updatedCart);
+                                    }}>Xóa</button>
+                                </td>
+                            </tr>
+                        ))}
                         <tr className="totalprice">
                             <td colSpan={3}>&nbsp;</td>
                             <td colSpan={2}>
-                                <div className='td-total-price'><p>Tổng giá tiền</p>
-                                    <span className="thick">$225.45</span></div>
+                                <div className='td-total-price'>
+                                    <p>Tổng giá tiền</p>
+                                    <span className="thick">${totalAmount.toFixed(2)}</span>
+                                </div>
                             </td>
                         </tr>
                         <tr className="checkoutrow">
                             <td colSpan={5} >
                                 <div className="checkout">
-                                    <button id="submitbtn" onClick={props.onClick}>Checkout Now</button>
-                                    <button id="submitbtn" onClick={props.onClick}>Xóa tất cả</button>
+                                    <button id="submitbtn" onClick={handleBuyClick}>Mua</button>
+                                    <button id="submitbtn" onClick={clearCart}>Xóa hết sản phẩm trong giỏ hàng</button>
+                                    <button id="submitbtn" onClick={onClick}>Đóng Cart</button>
                                 </div>
                             </td>
                         </tr>
                     </tbody>
                 </table>
-                </div>
-            </div>
-        
+                {/* Modal xác nhận mua */}
+                {showBuyConfirmation && (
+                    <div className="modal">
+                        <div className="modal-content">
+                            <h2>Xác nhận mua?</h2>
+                            <button onClick={confirmBuy}>Đồng ý</button>
+                            <button onClick={closeModal}>Hủy</button>
+                        </div>
+                    </div>
+                )}
 
-    )
+                {/* Modal thông báo mua thành công */}
+                {showPurchaseSuccess && (
+                    <div className="modal">
+                        <div className="modal-content">
+                            <h2>Mua thành công!</h2>
+                            <button onClick={closeModal}>Đóng</button>
+                        </div>
+                    </div>
+                )}
+            </div>
+        </div>
+    );
 }
 
-export default Cart
+export default Cart;
